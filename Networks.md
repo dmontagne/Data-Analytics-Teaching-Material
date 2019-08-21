f5c14a1c2bceb983b0a78374bf7d63c85e51628e
Danielle Montagne <danielle.montagne@duke.edu>
2019-08-21 13:02:48 -0400

    Network code and data

---
+++ Networks.md
@@ -0,0 +1,40 @@
+<style>
+
+
+.reveal section p {
+  color: black;
+  font-size: .7em;
+  font-family: 'Helvetica'; #this is the font/color of text in slides
+}
+
+
+.section .reveal .state-background {
+    background: white;}
+.section .reveal h1,
+.section .reveal p {
+    color: black;
+    position: relative;
+    top: 4%;}
+
+
+.wrap-url pre code {
+  word-wrap:break-word;
+}
+
+</style>
+
+Networks
+========================================================
+autosize: true
+transition: fade
+
+follow along with code: https://dmontagne.github.io/Data-Analytics-Teaching-Material/Networks_R_code.R 
+  
+Agenda:
+========================================================
+- Visualize a social network
+- Build a topic model
+
+
+
+
---
+++ network_analysis_code.R
@@ -0,0 +1,73 @@
+library(igraph)
+
+# Load in data 
+
+load("~/Documents/GitHub/Data-Analytics-Teaching-Material/Music Network Data/artist_edges.Rda")
+load("~/Documents/GitHub/Data-Analytics-Teaching-Material/Music Network Data/vertices_info.Rda")
+
+artist_network <- graph.data.frame(artist_edges[, -1], directed=F) # , vertices=vertices_info
+
+# Let's plot our network
+plot(artist_network,
+     layout = layout.fruchterman.reingold(artist_network),
+     vertex.size = .5,
+     vertex.label = V(artist_network)$simple_labels)
+
+# Right now it looks like a wall of text more than anything else.
+# How can we make this more readable?
+
+
+# Let's make a new variable in the igraph object that tells igraph to
+# only label those artists that are connected to 20 or more other artists.
+# In network lingo, this means that we only want labels for artists with
+# a "degree" of 20 or more.
+
+
+# To calculate the degree of each node (artist) in our network, we use the
+# `degree` function:
+degree(artist_network)
+
+# To get a vector of names for each node, we extract the `name` variable
+# from our `igraph` object:
+V(artist_network)$name
+
+
+# The information from the two calls above give us all the information we need
+# to make simpler labels. Let's add a new variable, `simple_labels`, to our igraph
+# object using the `ifelse` function:
+V(artist_network)$simple_labels <- ifelse(
+  # If an artist's degree is less than 20, don't give it a label (`NA`)
+  degree(artist_network) < 20, NA, 
+  # If an artist's degree is 20 or more, however, label it with the artist's
+  # name (stored in `V(artist_network)$name`)
+  V(artist_network)$name)
+
+
+# Now that we've build our `simple_labels` variable, let's tell `igraph` to
+# use this variable when plotting our network. To do this, set the `vertex.label`
+# argument to `simple_labels` in the `plot` function:
+plot(artist_network,
+     layout = layout.fruchterman.reingold(artist_network),
+     vertex.size = 1,
+     vertex.label = V(artist_network)$simple_labels)
+
+# It looks a lot better, though some names are still hard to read in the
+# middle of the network.
+
+V(artist_network)$popularity <- vertices_info$popularity
+V(artist_network)$transformed_popularity <- scale(vertices_info$popularity) + min(scale(vertices_info$popularity))*-1.5
+
+# Measures of the verteces
+betweenness(artist_network, v = V(artist_network), directed = F)
+closeness(artist_network, v = V(artist_network))
+
+# Whole graph measures
+edge_density(artist_network)
+transitivity(artist_network)
+
+# Clustering
+cluster_edge_betweenness(artist_network)
+
+
+
+
