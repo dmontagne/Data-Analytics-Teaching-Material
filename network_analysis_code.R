625cc699b55be737a47cf631789f4abb0d5d84bb
Danielle <daniellee.montagne@gmail.com>
2019-11-10 17:38:00 -0500

    add student code

--- network_analysis_code.R
+++ network_analysis_code.R
@@ -15,9 +15,6 @@ load("git_edges")
 download.file("https://github.com/dmontagne/Data-Analytics-Teaching-Material/blob/master/Music%20Network%20Data/vertices_info.Rda?raw=true", "git_vertices")
 load("git_vertices")
 
-download.file("https://github.com/dmontagne/Data-Analytics-Teaching-Material/blob/master/Music%20Network%20Data/lyrics_sample.Rda?raw=true", "git_lyrics")
-load("git_lyrics")
-
 artist_network <- graph.data.frame(artist_edges[, -1], directed=F) # , vertices=vertices_info
 
 # Let's plot our network
@@ -86,10 +83,39 @@ artist_groups %>%
 
 # Topic Modeling
 
+download.file("https://github.com/dmontagne/Data-Analytics-Teaching-Material/blob/master/lyric_stm_sample.rda?raw=true", "git_lyrics")
+load("git_lyrics")
 
+metadata_lyrics <- lyrics_sample %>% 
+  select(artist_id)
 
+process_lyrics <- textProcessor(lyrics_sample$lyrics, metadata = metadata_lyrics)
+# OR
+download.file("https://github.com/dmontagne/Data-Analytics-Teaching-Material/blob/master/process_lyrics.rda?raw=true", "git_process_lyrics")
+load("git_process_lyrics")
 
+prep_lyrics <- prepDocuments(process_lyrics$documents, process_lyrics$vocab, process_lyrics$meta)
+# OR
+download.file("https://github.com/dmontagne/Data-Analytics-Teaching-Material/blob/master/prep_lyrics.rda?raw=true", "git_prep_lyrics")
+load("git_prep_lyrics")
 
+which_k <- searchK(prep_lyrics$documents, prep_lyrics$vocab, K = seq(10, 50, by = 10),
+                   data = prep_lyrics$meta, verbose = T) # Takes a while to run
+
+lyrics_30 <-  stm(documents = prep_lyrics$documents,
+                  vocab = prep_lyrics$vocab,
+                  K = 30,
+                  data = prep_lyrics$meta,
+                  init.type = "Spectral",
+                  verbose = F)
+# OR
+
+download.file("https://github.com/dmontagne/Data-Analytics-Teaching-Material/blob/master/lyrics_stm.rda?raw=true", "git_stm_lyrics")
+load("git_stm_lyrics")
+
+plot.STM(lyrics_30)
+
+labelTopics(lyrics_30)
 
 
 
